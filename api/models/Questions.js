27f81b1c630f72bc7616cce56686ad3c1f20826e
EC2 Default User <ec2-user@ip-172-30-5-193.ec2.internal>
2018-03-14 15:46:52 +0000

    Initial commit with questions model

---
+++ api/models/Questions.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+    
+    text: String,
+    answers: [ {text: String, correct: Boolean} ],
+    sources: [
+        {url: String, citation: String}
+    ],
+    type: String,
+    categories: [],
+    contributor: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    stats: {
+        total_attempts: Number,
+        total_correct: Number
+    }
+
+});
+
+var Question = mongoose.model('Question', QuestionSchema);
+
+module.exports.Question = Question;
+
+module.exports.QuestionSchema = QuestionSchema;
